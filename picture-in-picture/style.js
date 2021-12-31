0169e87165cd657b84f4679c384e7d26ee490a4c
Vingsfire Devops <partha@vingsfire.com>
2021-12-31 23:49:13 +0530

    picture in picture

---
+++ picture-in-picture/style.js
@@ -0,0 +1,9 @@
+function togglePictureInPicture() {
+    if (document.pictureInPictureElement) {
+        document.exitPictureInPicture();
+    } else {
+        if (document.pictureInPictureEnabled) {
+            video.requestPictureInPicture();
+        }
+    }
+}
